44685c99a02d03eb007007dba81b5bf02793b410
vic <fuyong1208@163.com>
2016-04-29 17:30:27 +0800

    remove

--- base/src/main/java/cn/dreamfy/thread/CyclicBarrierTest.java
+++ base/src/main/java/cn/dreamfy/thread/CyclicBarrierTest.java
@@ -1,37 +1,43 @@
 package cn.dreamfy.thread;
+
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class CyclicBarrierTest {
 
-    public static void main(String[] args) {
-        ExecutorService service = Executors.newCachedThreadPool();
-        final CyclicBarrier cb = new CyclicBarrier(3);
-        for (int i = 0; i < 3; i++) {
-            Runnable runnable = new Runnable() {
-                public void run() {
-                    try {
-                        Thread.sleep((long) (Math.random() * 10000));
-                        System.out.println("线程" + Thread.currentThread().getName() +
-                                "即将到达集合地点1，当前已有" + (cb.getNumberWaiting() + 1) + "个已经到达，" + (cb.getNumberWaiting() == 2 ? "都到齐了，继续走啊" : "正在等候"));
-                        cb.await();
+	public static void main(String[] args) {
 
-                        Thread.sleep((long) (Math.random() * 10000));
-                        System.out.println("线程" + Thread.currentThread().getName() +
-                                "即将到达集合地点2，当前已有" + (cb.getNumberWaiting() + 1) + "个已经到达，" + (cb.getNumberWaiting() == 2 ? "都到齐了，继续走啊" : "正在等候"));
-                        cb.await();
-                        Thread.sleep((long) (Math.random() * 10000));
-                        System.out.println("线程" + Thread.currentThread().getName() +
-                                "即将到达集合地点3，当前已有" + (cb.getNumberWaiting() + 1) + "个已经到达，" + (cb.getNumberWaiting() == 2 ? "都到齐了，继续走啊" : "正在等候"));
-                        cb.await();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            };
-            service.execute(runnable);
-        }
-        service.shutdown();
-    }
+		ExecutorService service = Executors.newCachedThreadPool();
+		final CyclicBarrier cb = new CyclicBarrier(3);
+		for (int i = 0; i < 3; i++) {
+			Runnable runnable = new Runnable() {
+				public void run() {
+					try {
+
+						Thread.sleep((long) (Math.random() * 10000));
+						System.out.println("线程" + Thread.currentThread().getName() +
+								"即将到达集合地点1，当前已有" + (cb.getNumberWaiting() + 1) + "个已经到达，" + (cb.getNumberWaiting() == 2 ? "都到齐了，继续走啊" : "正在等候"));
+						// parties 启动此 barrier 的参与者数目
+						// numberWait 当前在 barrier 等待的参与者数目
+						System.out.println("parties:" + cb.getParties() + " numberWait:" + cb.getNumberWaiting());
+						cb.await();
+
+						Thread.sleep((long) (Math.random() * 10000));
+						System.out.println("线程" + Thread.currentThread().getName() +
+								"即将到达集合地点2，当前已有" + (cb.getNumberWaiting() + 1) + "个已经到达，" + (cb.getNumberWaiting() == 2 ? "都到齐了，继续走啊" : "正在等候"));
+						cb.await();
+						Thread.sleep((long) (Math.random() * 10000));
+						System.out.println("线程" + Thread.currentThread().getName() +
+								"即将到达集合地点3，当前已有" + (cb.getNumberWaiting() + 1) + "个已经到达，" + (cb.getNumberWaiting() == 2 ? "都到齐了，继续走啊" : "正在等候"));
+						cb.await();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			};
+			service.execute(runnable);
+		}
+		service.shutdown();
+	}
 }
